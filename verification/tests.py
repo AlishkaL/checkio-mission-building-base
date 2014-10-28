1d4c40da4f1529f6863a398d989da49e1919c56f
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-10-28 20:19:40 +0300

    update test

--- verification/tests.py
+++ verification/tests.py
@@ -19,14 +19,14 @@ def prepare_test(test, answer, middle_code="\n", show_code=None):
 
 TESTS = {
     "1. Init": [
-        prepare_test("", "", "Building((1, 1), (2, 2))"),
-        prepare_test("", "", "Building((1, 1), (2, 2), 10)"),
-        prepare_test("", "", "Building((0.54345, 1.12313), (2./6, 3.3 * 5), 1./2)")
+        prepare_test("", "", "Building(1, 1, 2, 2)"),
+        prepare_test("", "", "Building(1, 1, 2, 2, 10)"),
+        prepare_test("", "", "Building(0.54345, 1.12313, 2./6, 3.3 * 5, 1./2)")
     ],
     "2. Str": [
-        prepare_test("str(Building((1, 1), (2, 2))",
-                     "Building from [1, 1] to [2, 2]. Height 10.",),
-        prepare_test("str(Building((0.2, 1), (2, 2.2), 3.5)",
-                     "Building from [0.2, 1] to [2, 2.2]. Height 3.5.",),
+        prepare_test("str(Building(1, 1, 2, 2)",
+                     "Building from [1, 1] with size 2 by 2. Height 10.",),
+        prepare_test("str(Building(0.2, 1, 2, 2.2, 3.5)",
+                     "Building from [0.2, 1] with size 2 by 2.2. Height 3.5.",),
     ]
 }
