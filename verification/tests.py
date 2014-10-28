c3cb05f42301e5453f42300921b8c57e96c9439e
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-10-28 21:16:52 +0300

    various fixes

--- verification/tests.py
+++ verification/tests.py
@@ -5,28 +5,29 @@ if not "Building" in USER_GLOBAL:
 Building = USER_GLOBAL['Building']
 """
 
-run_test = """RET['code_result'] = {}
+run_test = """
+RET['code_result'] = {}
 """
 
-def prepare_test(test, answer, middle_code="\n", show_code=None):
+def prepare_test(test="", answer="", middle_code="\n", show_code=None):
     if show_code is None:
         show_code = middle_code + test
-    return {"test_code": {"python-3": init_code + middle_code + run_test.format(test),
-                          "python-27": init_code + middle_code + run_test.format(test)},
+    return {"test_code": {"python-3": init_code + middle_code + (run_test.format(test) if test else ""),
+                          "python-27": init_code + middle_code + (run_test.format(test) if test else "")},
             "show": {"python-3": show_code,
                      "python-27": show_code},
             "answer": answer}
 
 TESTS = {
     "1. Init": [
-        prepare_test("", "", "Building(1, 1, 2, 2)"),
-        prepare_test("", "", "Building(1, 1, 2, 2, 10)"),
-        prepare_test("", "", "Building(0.54345, 1.12313, 2./6, 3.3 * 5, 1./2)")
+        prepare_test(middle_code="Building(1, 1, 2, 2)"),
+        prepare_test(middle_code="Building(1, 1, 2, 2, 10)"),
+        prepare_test(middle_code="Building(0.54345, 1.12313, 2./6, 3.3 * 5, 1./2)")
     ],
     "2. Str": [
-        prepare_test("str(Building(1, 1, 2, 2)",
-                     "Building from [1, 1] with size 2 by 2. Height 10.",),
-        prepare_test("str(Building(0.2, 1, 2, 2.2, 3.5)",
-                     "Building from [0.2, 1] with size 2 by 2.2. Height 3.5.",),
+        prepare_test("str(Building(1, 1, 2, 2))",
+                     "Building at [1, 1]. Size 2 by 2. Height 10.",),
+        prepare_test("str(Building(0.2, 1, 2, 2.2, 3.5))",
+                     "Building at [0.2, 1]. Size 2 by 2.2. Height 3.5.",),
     ]
 }
